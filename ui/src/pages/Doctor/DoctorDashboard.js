b9bb9afbcd984a20e227fa2ac1ccff3098fb0ec3
ridhampatel14 <rydhamik@gmail.com>
2023-11-15 15:06:20 -0500

    added doctor form and checking profile functionality

--- ui/src/pages/Doctor/DoctorDashboard.js
+++ ui/src/pages/Doctor/DoctorDashboard.js
@@ -1,11 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button } from '@mui/material/';
 import { doSignOut } from '../../firebase/functions';
 import { AuthContext } from '../../firebase/Auth';
 import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { Typography, CircularProgress } from '@mui/material';
 
 export default function DoctorDashboard() {
 	const { currentUser } = useContext(AuthContext);
+	const [hasProfile, setHasProfile] = useState(false);
+	const [loading, setLoading] = useState(true);
+
+	const fetchData = async (email) => {
+		try {
+			const url = 'http://localhost:3002/doctors/' + email;
+			let ans = await axios.get(url);
+			console.log(email);
+			console.log('*********************profile:- ', ans.data);
+			setHasProfile(ans.data);
+			setLoading(false);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	useEffect(() => {
+		console.log('in load');
+		if (currentUser) {
+			fetchData(currentUser.email);
+		}
+	}, [currentUser]);
 
 	if (!currentUser) {
 		return <Navigate to={`/`} />;
@@ -14,16 +38,64 @@ export default function DoctorDashboard() {
 	return (
 		<div className="main">
 			<div>
-				<h1>doctors dashboard</h1>
-				<Button
-					variant="outlined"
-					color="secondary"
-					onClick={doSignOut} // Call handleSignUp on button click
-					style={{ marginTop: '1em' }}
-				>
-					Sign Out
-				</Button>
+				{loading ? (
+					<div style={{ display: 'flex', alignItems: 'center' }}>
+						<CircularProgress size={40} color="secondary" />
+						<Typography variant="body1" style={{ marginLeft: 10 }}>
+							Loading...
+						</Typography>
+					</div>
+				) : hasProfile ? (
+					<>
+						<h1>doctors dashboard</h1>
+						<h2>{currentUser.email}</h2>
+						<Button
+							variant="outlined"
+							color="secondary"
+							onClick={doSignOut} // Call handleSignUp on button click
+							style={{ marginTop: '1em' }}
+						>
+							Sign Out
+						</Button>
+					</>
+				) : (
+					<Navigate to={`/DoctorDetailsForm`} />
+				)}
 			</div>
 		</div>
 	);
+
+	// return (
+	// 	<div className="main">
+	// 		<div>
+	// 			{ loading
+	// 			? (
+	// 				<div style={{ display: 'flex', alignItems: 'center' }}>
+	// 					<CircularProgress size={40} color="secondary" />
+	// 					<Typography variant="body1" style={{ marginLeft: 10 }}>
+	// 						Loading...
+	// 					</Typography>
+	// 				</div>
+	// 			)
+	// 			:
+	// 			(hasProfile ? (
+	// 				<>
+	// 					<h1>doctors dashboard</h1>
+	// 					<Button
+	// 						variant="outlined"
+	// 						color="secondary"
+	// 						onClick={doSignOut} // Call handleSignUp on button click
+	// 						style={{ marginTop: '1em' }}
+	// 					>
+	// 						Sign Out
+	// 					</Button>
+	// 				</>
+	// 			) : (
+	// 				<Navigate to={`/DoctorDetailsForm`} />
+	// 			))
+
+	// 		}
+	// 		</div>
+	// 	</div>
+	// );
 }
