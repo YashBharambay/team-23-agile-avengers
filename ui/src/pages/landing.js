6efe47e7c5800b37029ca977bb429deea404bed3
ridhampatel14 <rydhamik@gmail.com>
2023-12-03 22:48:15 -0500

    changed landing page UI

--- ui/src/pages/landing.js
+++ ui/src/pages/landing.js
@@ -1,29 +1,38 @@
-import React, { useContext } from 'react';
-import { Box, Button, Container, Grid, Paper, Typography } from '@mui/material';
+import React from 'react';
+import {
+	Box,
+	Button,
+	Container,
+	Grid,
+	Divider,
+	Paper,
+	Typography,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
-//import { Navigate } from 'react-router-dom';
 import './landing.css';
-//import { AuthContext } from '../firebase/Auth';
-
-// Defined color constants
 const PRIMARY_COLOR = 'primary';
-const BLACK_COLOR = 'black';
 
-//Wrote a fucntion that renders the button so that we can manage and use this everywhere.
 function RenderButton(label, color, to) {
 	return (
 		<Box>
-			<Button variant="contained" color={color} size="large">
-				<Link className="showlink" to={to}>
-					{label}
-				</Link>
+			<Button
+				variant="contained"
+				color={color}
+				size="large"
+				component={Link}
+				to={to}
+				style={{
+					borderRadius: 50,
+					boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+				}}
+			>
+				{label}
 			</Button>
 		</Box>
 	);
 }
-export default function LandingPage() {
-	//const { currentUser } = useContext(AuthContext);
 
+export default function LandingPage() {
 	const pageStyle = {
 		background: 'linear-gradient(135deg, #64B5F6 0%, #1976D2 100%)',
 		minHeight: '100vh',
@@ -31,33 +40,61 @@ export default function LandingPage() {
 		justifyContent: 'center',
 		alignItems: 'center',
 	};
+
 	return (
 		<div style={pageStyle}>
 			<Paper elevation={3} style={{ padding: 20, margin: 20 }}>
-				<Container maxWidth="lg">
-					<Grid container spacing={4} alignItems="center" justify="center">
-						<Grid item xs={12}>
-							<Typography variant="h2" align="center" gutterBottom>
+				<Container maxWidth="md">
+					<Grid container spacing={4}>
+						<Grid item xs={12} md={6}>
+							<Typography
+								variant="h4"
+								align="left"
+								gutterBottom
+								sx={{
+									color: '#0091c4',
+									fontSize: '2.3rem',
+									fontWeight: 'bold',
+								}}
+							>
 								Enriching End of Life Care with Technology
 							</Typography>
-						</Grid>
-						<Grid item xs={12} sm={6}>
-							<Typography variant="h6" align="center" paragraph>
+							<br />
+							<Typography
+								variant="h6"
+								align="left"
+								paragraph
+								sx={{
+									color: 'black',
+									fontSize: '1.3rem',
+									fontWeight: 'bold',
+								}}
+							>
 								Welcome to our platform dedicated to improving end-of-life care
 								through technology.
 							</Typography>
-						</Grid>
-						<Grid item xs={12} sm={6}>
-							<Typography variant="body1" align="center" paragraph>
+							<br />
+							{/* <Typography variant="body1" align="left" paragraph>
 								In this journey, we aim to make a difference in end-of-life care
 								by leveraging cutting-edge technology. Join us in this mission
 								to provide comfort, support, and dignity to those in need.
-							</Typography>
+							</Typography> */}
+							<div>
+								<Box mt={2} align="center">
+									<Box m={2}></Box>
+									{RenderButton('Doctor', PRIMARY_COLOR, '/DocLogin')}
+
+									<Box m={2}></Box>
+									{RenderButton('For Patients', PRIMARY_COLOR, '/PatLogin')}
+								</Box>
+							</div>
 						</Grid>
-						<Grid item xs={12} align="center">
-							{RenderButton('Doctor', PRIMARY_COLOR, '/DocLogin')}
-							<Box m={2}></Box>
-							{RenderButton('For Patients', PRIMARY_COLOR, '/PatLogin')}
+						<Grid item xs={12} md={6}>
+							<img
+								src="/Online.jpg"
+								alt="doctor logo"
+								style={{ width: '100%', height: 'auto' }}
+							/>
 						</Grid>
 					</Grid>
 				</Container>
